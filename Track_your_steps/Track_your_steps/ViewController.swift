530631e755efe8ef98ebc1bd1a5fe9696fbded3b
Sunita Nhemafuki <snhemafu@purdue.edu>
2021-08-05 18:30:05 -0500

    Initial Commit

---
+++ Track_your_steps/Track_your_steps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Track_your_steps
+//
+//  Created by Sunita Nhemafuki on 8/5/21.
+//  Copyright © 2021 Sunita Nhemafuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
